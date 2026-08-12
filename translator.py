7e297e58dfea1fc85d66a6ccc9fc6f9d3692be5c
MuazAwan3288 <muazawan3288@gmail.com>
2026-08-12 16:47:41 +0500

    Implement translation model

---
+++ translator.py
@@ -0,0 +1,47 @@
+#This file loads the model and does translation
+
+import torch
+from transformers import AutoTokenizer, AutoModelForSeq2SeqLM
+from utils.helper import get_model
+
+class Translator:
+    def __init__(self):
+#we keep models in memory so we dont download again
+#   we use dictionary to store models with their name as key
+        self.models = {}
+        self.tokenizers = {}
+
+        if torch.cuda.is_availaable():
+            self.device = "cuda"
+        else:
+            self.device = "cpu"
+ # print("Using device:", self.device) # for checking
+# this loads model if not already loaded
+
+def load_model(self, model_name):
+    if model_name not in self.models:
+        tok = AutoTokenizer.from_pretrained(model_name)
+        mod = AutoModelForSeq2SeqLM.from_pretrained(model_name)
+        mod = mod.to(self.device)
+        self.tokenizers[model_name] = tok
+        self.models[model_name] =mod
+
+        return self.tokenizers[model_name], self.models[model_name]
+
+#main fuction tp translate text
+
+def do_translation(self, text, from_lang, to_lang):
+    model_name = get_model(from_lang, to_lang)
+
+    try:
+        tokenizer, model = self.load_model(model_name)
+ #covert text to tokens
+        inputs = tokenizer(text, return_tensors = "pt", padding = True, truncation = True)
+        inputs = inputs.to(self.device)
+#generate translation
+        with torch.no_grad():
+            outputs = model.generate(**inputs, max_length = 512)
+#convert token back to text
+        result = tokenizer.decode(outputs[0], skip_special_tokens = True) 
+    except:
+        return "Sorry translation failed. Please try agaqin later"
---
+++ utils/helper.py
@@ -0,0 +1,50 @@
+#this file has some small functions that we need in the app
+# we make a list of languages with their short codes 
+# huggingface modelss need codes like en, fr, de etc 
+
+language_list = {
+    "English": "en",
+    "French": "fr",
+    "German": "de",
+    "Spanish": "es",
+    "Italian": "it",
+    "Portuguese": "pt",
+    "Urdu": "ur"
+    }
+
+#this function will return all languages name for dropdown menu
+
+def get_language():
+    name = []
+    for i in language_list:
+        name.append(i)
+    return name
+
+#this function checks if user inout is ok or not 
+
+def check_input(text, from_lang, to_lang):
+    if text == "":
+        return False, "Please Write Some Thing to Translate"
+    if text.isspace() == True:
+        return False, "Please Write Some Thing to Translate"
+
+    if from_lang not in language_list:
+        return False, "Source Language is not Available"
+    if to_lang not in language_list:
+        return False, "Target Language is not Available"
+
+    if from_lang == to_lang:
+        return False, "Please Select Different Language"
+    #IF ALL Okay
+    return True, "OK"
+
+#This function makes the model name for huggingface
+#example: Helsinki-NLP/opus-mt-en-fr
+
+def get_model(from_lang, to_lang):
+    code1 = language_list[from_lang]
+    code2 = language_list[to_lang]
+
+    model_name = f"Helsinki-NLP/opus-mt-"+ code1 + "-" + code2
+    return model_name
+
